9955703fb25ff1a4e338f532b043b58d0a328298
gabboas <gmaia@outlook.ie>
2024-01-24 00:24:40 +0000

    Explaination of how to reproduce something in js.

---
+++ src/js/mobilemenu.js
@@ -0,0 +1,20 @@
+/* 
+Questions:
+ 1how does a menu come from the 3 lines from top to bottom;
+ 2how a pic changes its focus;
+ 3maybe it is the same case of 1st - menu from right to left;
+
+click on the menubar;
+ - [ ] Change the appearance of something when it is clicked
+
+the menu will become an X;
+ - [ ] the top line goes twards down on diagonal exile
+ - [ ] Middle one disappears
+ - [ ] the bottom line goes upwards on diagonal
+
+
+then the whole nav menu will drop down with different options for next steps;
+ - [ ] The menu occupies the whole screen
+ - [ ] Expand and hide the menu
+ - [ ] Gradual animation
+*/
